fe4fb729c04eb0f30b8e04413423e791dd15d6fc
aminulhasan142 <mahshoukhin142@gmail.com>
2024-06-22 22:41:28 +0600

    finished setup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,13 +6,23 @@ const button = ({ className, href, onClick, children, px, white }) => {
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
+  const spanClasses = "relative z-10";
+
   const renderButton = () => (
     <button className={classes}>
-      <span>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+
+  const renderLink = () => {
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>;
+  };
+
+  return href ? renderLink() : renderButton();
 };
 
 export default button;
